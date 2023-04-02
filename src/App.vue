71b853f3d7fce45020d90f86af92279c104d7621
alfiya555 <87981671+alfiya555@users.noreply.github.com>
2023-04-03 03:48:19 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Header />
+    <div class="wrapper">
+      <UsersView />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import UsersView from './components/UsersView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    UsersView,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  color: #696969;
+}
+
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 77px;
+  width: 100%;
+  height: 100%;
+  padding: 30px 60px;
+}
+</style>
